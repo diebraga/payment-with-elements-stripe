2e72bdff1ea0efc742bfa883a3cbce2e4d356a17
diebraga <diebraga0055@gmail.com>
2021-04-01 01:29:34 +0000

    delete layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,18 @@ import { NextPage } from 'next'
 import { Elements } from '@stripe/react-stripe-js'
 import getStripe from '../utils/get-stripejs'
 
-import Layout from '../components/Layout'
 import ElementsForm from '../components/ElementsForm'
 
 const DonatePage: NextPage = () => {
   return (
-    <Layout title="Donate with Elements | Next.js + TypeScript Example">
+    <div className="container">
       <div className="page-container">
         <h1>Pagamento ingresso na plataforma</h1>
         <Elements stripe={getStripe()}>
           <ElementsForm />
         </Elements>
       </div>
-    </Layout>
+    </div>
   )
 }
 
